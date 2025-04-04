40c71d8b79b8d803a4fa4a3ceb691dceea821e13
Talison Azzini <talison.azzini@gmail.com>
2025-04-04 12:52:47 -0300

    Criação da validação do formulario

---
+++ src/main/resources/static/validar.js
@@ -0,0 +1,129 @@
+$("#frmLogin").validate({
+    rules: {
+        login: {
+            required: true
+        },
+        senha: {
+            required: true,
+            minlength: 6,
+            maxlength: 12
+        }
+    },
+    messages: {
+        login: {
+            required: "O campo login é obrigatório"
+        },
+        senha: {
+            required: "O campo senha é obrigatório",
+            minlength: "A senha deve ter ao menos 6 caracteres",
+            maxlength: "A senha deve ter no máximo 12 caracteres"
+        }
+    }
+});
+
+$("#frmUsuario").validate({
+    rules: {
+        nome: {
+            required: true,
+            maxlength: 15
+        },
+        nascimento: {
+            required: true,
+            date: true
+        },
+        telefone: {
+            required: true,
+            minlength: 14
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        login: {
+            required: true
+        },
+        senha: {
+            required: true,
+            minlength: 6,
+            maxlength: 12
+        }
+    },
+    messages: {
+        nome: {
+            required: "O campo nome é obrigatório",
+            maxlength: "O campo deve ter no máximo 15 caracteres."
+        },
+        nascimento: {
+            required: "O campo data de nascimento é obrigatório",
+            date: "O campo deve ter formato dd/mm/aaaa"
+        },
+        telefone: {
+            required: "O campo telefone é obrigatório",
+            minlength: "O campo deve ter formato (xx) xxxxx-xxxx"
+        },
+        email: {
+            required: "O campo e-mail é obrigatório",
+            email: "O campo deve ter formato user@provedor"
+        },
+        login: {
+            required: "O campo login é obrigatório"
+        },
+        senha: {
+            required: "O campo senha é obrigatório",
+            minlength: "A senha deve ter ao menos 6 caracteres",
+            maxlength: "A senha deve ter no máximo 12 caracteres"
+        }
+    }
+});
+
+$("#frmProduto").validate({
+    rules: {
+        nome: {
+            required: true,
+            maxlength: 25
+        },
+        valorUnitario: {
+            required: true
+        },
+        quantidade: {
+            required: true,
+            minlength: 0,
+            maxlength: 200
+        },
+        categoria: {
+            required: true,
+            maxlength: 15
+        }
+    },
+    messages: {
+        nome: {
+            required: "O campo nome do produto é obrigatório.",
+            maxlength: "O campo deve ter no máximo 15 caracteres."
+        },
+        valorUnitario: {
+            required: "O campo valor unitário é obrigatório."
+        },
+        quantidade: {
+            required: "O campo quantidade é obrigatório.",
+            minlength: "O campo deve ter apenas números positivos.",
+            maxlength: "A quantidade maxima é de 200."
+        },
+        categoria: {
+            required: "O campo categoria é obrigatório.",
+            maxlength: "O campo deve ter no maximo 15 caracteres."
+        }
+    }
+});
+
+$("#frmPedido").validate({
+    rules: {
+        quantidade: {
+            minlength: 0
+        }
+    },
+    messages: {
+        quantidade: {
+            minlength: "Este campo so aceita números positivos."
+        }
+    }
+});
