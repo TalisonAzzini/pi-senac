909004fece6229e3a6e7c12ce4b5f8ca067bf4e3
Talison Azzini <talison.azzini@gmail.com>
2025-04-06 15:43:47 -0300

    Criação da entidade Usuario

--- src/main/java/com/senac/pi/model/Usuario.java
+++ src/main/java/com/senac/pi/model/Usuario.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.senac.pi.model;
 
-/**
- *
- * @author talis
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.sql.Date;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Date nascimento;
+    private String telefone;
+    private String email;
+    private String login;
+    private String senha;
 }
